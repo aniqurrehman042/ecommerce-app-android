13eda690342ec214db6d634c61c04e4896a13112
Aniq Ur Rehman <aniqurrehman042@gmail.com>
2020-07-02 03:09:22 -0700

    Added GettingStartedActivity

--- app/src/main/java/com/sahoolatkar/sahoolatkar/ui/GettingStartedActivity.kt
+++ app/src/main/java/com/sahoolatkar/sahoolatkar/ui/GettingStartedActivity.kt
@@ -3,10 +3,12 @@ package com.sahoolatkar.sahoolatkar.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sahoolatkar.sahoolatkar.R
+import com.sahoolatkar.sahoolatkar.utils.UIUtils
 
 class GettingStarted : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_getting_started)
+        UIUtils.setFullScreen(window)
     }
 }
